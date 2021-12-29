5062149fb843747356e2b2849d0ec75bfd489688
João Guilherme Cobo <joaocobo20@gmail.com>
2021-12-29 16:42:52 -0300

    Home Pronta e Ediçao na API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,97 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app id="home">
+    <v-container>
+      <v-card elevation="8" shaped>
+        <v-card-title>Bem vindo</v-card-title>
+        <v-form>
+          <v-card-text
+            >Olá, este é nosso projeto de Vuetify consumindo API's</v-card-text
+          >
+        </v-form>
+      </v-card>
+      <v-row justify="center" class="mx-auto mt-5">
+        <v-dialog v-model="dialog" persistent max-width="290">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" dark v-bind="attrs" v-on="on">
+              Criadores
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title class="text-h5"> Time Solinftec </v-card-title>
+            <v-card-text
+              >Nosso grupo é composto por 5 membros:
+              <ul>
+                <li>João Cobo</li>
+                <li>Luana Miki</li>
+                <li>Lucas Rodrigo</li>
+                <li>Weslley Costa</li>
+                <li>Rodrigo José</li>
+              </ul>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="green darken-1" text @click="dialog = false">
+                ok
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-row>
+      <v-timeline>
+        <v-timeline-item v-for="n in 1" :key="n" color="red lighten-2" large>
+          <template v-slot:opposite>
+            <span>Inicio</span>
+          </template>
+          <v-card class="elevation-2" color="red lighten-4">
+            <v-card-title class="text-h5"> O Inicio </v-card-title>
+            <v-card-text>
+              Foi criado um repositório no Github para os membros da equipe
+              efetuarem o Fork.
+            </v-card-text>
+          </v-card>
+        </v-timeline-item>
+        <v-timeline-item v-for="n in 1" :key="n" color="red lighten-2" large>
+          <template v-slot:opposite>
+            <span>Criação</span>
+          </template>
+          <v-card class="elevation-2" color="deep-purple lighten-4">
+            <v-card-title class="text-h5"> A Criação </v-card-title>
+            <v-card-text>
+              O projeto base desenvolvido, é um menu onde contem os nomes dos
+              integrantes do projeto.
+            </v-card-text>
+          </v-card>
+        </v-timeline-item>
+        <v-timeline-item v-for="n in 1" :key="n" color="red lighten-2" large>
+          <template v-slot:opposite>
+            <span>Objetivo</span>
+          </template>
+          <v-card class="elevation-2" color="green lighten-3">
+            <v-card-title class="text-h5"> O Objetivo </v-card-title>
+            <v-card-text>
+              O projeto tem como objetivo desenvolver o espirito criativo de
+              cada membro da equipe, cada um terá que desenvolver uma tela com o
+              consumo de uma API.
+            </v-card-text>
+          </v-card>
+        </v-timeline-item>
+      </v-timeline>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      dialog: false,
+    };
   },
 };
 </script>
+
+<style>
+#home {
+  background-color: powderblue;
+}
+</style>
--- src/views/Joao.vue
+++ src/views/Joao.vue
@@ -1,92 +1,96 @@
 <template>
-  <v-app>
-    <v-card
-      elevation="10"
-      shaped
-      width="400"
-      class="mx-auto mt-5"
-      color="amber lighten-3"
-    >
-      <v-card-title
-        ><h1 class="display-1">League of Legends API</h1></v-card-title
+  <v-app id="joao">
+    <div>
+      <v-card
+        elevation="10"
+        shaped
+        width="400"
+        class="mx-auto mt-5"
+        color="blue lighten-2"
       >
-      <v-card-subtitle>
-        <h2>Digite seu nick</h2>
-      </v-card-subtitle>
-      <v-form ref="form" v-model="valid" lazy-validation>
-        <v-container>
-          <v-text-field
-            v-model="name"
-            :counter="40"
-            :rules="nameRules"
-            label="Nick"
-            v-on:blur="validaNome"
-            required
-          ></v-text-field>
-          <v-btn
-            :disabled="!valid"
-            color="success"
-            class="mr-4"
-            @click="validate"
-          >
-            Pesquisar
-          </v-btn>
-        </v-container>
-      </v-form>
-    </v-card>
-
-    <v-card class="mx-auto mt-5" elevation="5" v-show="exibir">
-      <v-container>
-        <v-row justify="space-around">
-          <v-card width="400">
-            <v-img
-              height="200px"
-              src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1f910d45-3c3a-44a3-ac62-03f42de5c1ec/d7ndlad-c950cfa3-9e41-4a85-ad66-18386928f178.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzFmOTEwZDQ1LTNjM2EtNDRhMy1hYzYyLTAzZjQyZGU1YzFlY1wvZDduZGxhZC1jOTUwY2ZhMy05ZTQxLTRhODUtYWQ2Ni0xODM4NjkyOGYxNzguanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.MKnNQbj-BGsMc2h1oj0VdlotJVHash5wQpG4PLouzSo"
+        <v-card-title
+          ><h1 class="display-1">League of Legends API</h1></v-card-title
+        >
+        <v-card-subtitle>
+          <h2>Digite seu nick</h2>
+        </v-card-subtitle>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-container>
+            <v-text-field
+              v-model="name"
+              :counter="40"
+              :rules="nameRules"
+              label="Nick"
+              v-on:blur="validaNome"
+              required
+            ></v-text-field>
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              @click="validate"
             >
-              <v-app-bar flat color="rgba(0, 0, 0, 0)">
-                <v-spacer></v-spacer>
-              </v-app-bar>
+              Pesquisar
+            </v-btn>
+          </v-container>
+        </v-form>
+      </v-card>
 
-              <v-card-title class="white--text mt-8">
-                <v-avatar size="56">
-                  <img :src="image" />
-                </v-avatar>
-                <p class="ml-3">{{ summonerName }}</p>
-              </v-card-title>
-            </v-img>
+      <v-card class="mx-auto mt-5" elevation="10" v-show="exibir" id="teste">
+        <v-container>
+          <v-row justify="space-around">
+            <v-card width="400">
+              <v-img
+                height="200px"
+                src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1f910d45-3c3a-44a3-ac62-03f42de5c1ec/d7ndlad-c950cfa3-9e41-4a85-ad66-18386928f178.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzFmOTEwZDQ1LTNjM2EtNDRhMy1hYzYyLTAzZjQyZGU1YzFlY1wvZDduZGxhZC1jOTUwY2ZhMy05ZTQxLTRhODUtYWQ2Ni0xODM4NjkyOGYxNzguanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.MKnNQbj-BGsMc2h1oj0VdlotJVHash5wQpG4PLouzSo"
+              >
+                <v-app-bar flat color="rgba(0, 0, 0, 0)">
+                  <v-spacer></v-spacer>
+                </v-app-bar>
 
-            <v-card-text>
-              <v-row>
-                <v-card-text v-model="Level">Level: {{ Level }}</v-card-text>
-              </v-row>
-              <v-row>
-                <v-card-text v-model="UltimoChampion"
-                  >Ultimo Champion jogado: {{ UltimoChampion }}</v-card-text
-                >
-              </v-row>
-              <v-row>
-                <v-card-text v-model="Maestria"
-                  >Maestria: {{ Maestria }}</v-card-text
-                >
-              </v-row>
-              <v-row>
-                <v-card-text v-model="UltimoJogo"
-                  >Ultima Jogada: {{ UltimoJogo }}</v-card-text
-                >
-              </v-row>
-              <v-row>
-                <v-card-text v-model="Tier">Tier: {{ Tier }}</v-card-text>
-              </v-row>
-            </v-card-text>
-          </v-card>
-        </v-row>
-      </v-container>
-    </v-card>
-    <v-footer padless app>
-      <v-col class="text-center" cols="12">
-        {{ new Date().getFullYear() }} — <strong>Cobol API</strong>
-      </v-col>
-    </v-footer>
+                <v-card-title class="white--text mt-8">
+                  <v-avatar size="56">
+                    <img :src="image" />
+                  </v-avatar>
+                  <p class="ml-3">{{ summonerName }}</p>
+                </v-card-title>
+              </v-img>
+
+              <v-card-text>
+                <v-row>
+                  <v-card-text v-model="Level">Level: {{ Level }}</v-card-text>
+                </v-row>
+                <v-row>
+                  <v-card-text v-model="UltimoChampion"
+                    >Ultimo Champion jogado: {{ UltimoChampion }}</v-card-text
+                  >
+                </v-row>
+                <v-row>
+                  <v-card-text v-model="Maestria"
+                    >Maestria: {{ Maestria }}</v-card-text
+                  >
+                </v-row>
+                <v-row>
+                  <v-card-text v-model="UltimoJogo"
+                    >Ultima Jogada: {{ UltimoJogo }}</v-card-text
+                  >
+                </v-row>
+                <v-row>
+                  <v-card-text v-model="Tier">Tier: {{ Tier }}</v-card-text>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-row>
+        </v-container>
+      </v-card>
+      <v-footer padless app>
+        <v-col class="text-center" cols="12">
+          {{ new Date().getFullYear() }} —
+          <strong style="padding-right: 15px">Cobol API</strong>
+          <a href="https://developer.riotgames.com/apis">Api's Usadas</a>
+        </v-col>
+      </v-footer>
+    </div>
   </v-app>
 </template>
 
@@ -179,3 +183,17 @@ export default {
   },
 };
 </script>
+
+<style>
+#joao {
+  background-image: url("https://images6.alphacoders.com/651/651136.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+#teste {
+  background-image: url("https://i.pinimg.com/originals/be/00/69/be006924ee1c307ec8836edc809efe77.png");
+  background-size: cover;
+  background-position: left;
+}
+</style>
